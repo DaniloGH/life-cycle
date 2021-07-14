37ef85f05d4db2bb26e7961bb22351bcc41f9f91
Danilo Miguel <danilomsouza@fac.pe.senac.br>
2021-07-14 15:36:22 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+	state = {
+		click: 'ainda não clicou'
+	}
+	componentDidMount(){
+		console.log('[App] - Apareceu')
+	}
+	handleClick(){
+		this.setState({
+			click: 'clicou'
+		})
+	}
+
+	render() {
+		console.log('[App] - Renderizou')
+		return (
+			<div>
+				Olá, {this.props.name}, você {this.state.click}.
+				<button onClick={this.handleClick.bind(this)}>Click</button>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App name={"Danilo Miguel"} />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
